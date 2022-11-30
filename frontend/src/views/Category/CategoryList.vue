b54f0b29f1594312243f647d2bd72a9b65086152
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2022-11-30 21:14:58 +0100

    css buttons dashboard categories

--- frontend/src/views/Category/CategoryList.vue
+++ frontend/src/views/Category/CategoryList.vue
@@ -1,18 +1,21 @@
 <template>
-    <router-link to="/dashboard/categories/create">
-        <button>CREATE</button>
-    </router-link>
-    <button @click="updateCategory()">UPDATE</button>
-    <button @click="deleteCategory()">DELETE</button>
-    <DataTable class="display" :options="{ select: true }" :columns="columns" :data="state.categories" ref="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Photo</th>
-            </tr>
-        </thead>
-    </DataTable>
+    <div class="categoriList">
+        <h1>Categories</h1>
+        <router-link to="/dashboard/categories/create">
+            <button class="pulse create">CREATE</button>
+        </router-link>
+        <button @click="updateCategory()" class="pulse update">UPDATE</button>
+        <button @click="deleteCategory()" class="pulse delete">DELETE</button>
+        <DataTable class="display" :options="{ select: true }" :columns="columns" :data="state.categories" ref="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Photo</th>
+                </tr>
+            </thead>
+        </DataTable>
+    </div>
 </template>
 
 <script>
@@ -77,4 +80,66 @@ export default {
 
 <style lang="scss">
 @import 'datatables.net-dt';
+
+.categoriList {
+    h1 {
+        text-align: center;
+    }
+
+    .pulse:hover,
+    .pulse:focus {
+        animation: pulse 1s;
+        box-shadow: 0 0 0 2em transparent;
+    }
+
+    @keyframes pulse {
+        0% {
+            box-shadow: 0 0 0 0 var(--hover);
+        }
+    }
+
+    $colors: (
+        create: green,
+        update: blue,
+        delete: red,
+    );
+
+@each $button,
+$color in $colors {
+    .#{$button} {
+        --color: #{$color};
+        --hover: #{adjust-hue($color, 45deg)};
+    }
+}
+
+button {
+    color: var(--color);
+    transition: 0.25s;
+
+    &:hover,
+    &:focus {
+        border-color: var(--hover);
+        color: #fff;
+    }
+}
+
+button {
+    background: none;
+    border: 2px solid;
+    font: inherit;
+    line-height: 1;
+    margin: 0.5em;
+    padding: 1em 2em;
+}
+
+h1 {
+    font-weight: 400;
+}
+
+code {
+    color: #e4cb58;
+    font: inherit;
+}
+
+}
 </style>
