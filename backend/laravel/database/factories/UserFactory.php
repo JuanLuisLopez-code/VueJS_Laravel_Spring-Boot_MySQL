2f2576173367386a08fd76c2c0fec1a499f43dba
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2022-12-05 19:32:56 +0100

    user model, factory and seeder

--- backend/laravel/database/factories/UserFactory.php
+++ backend/laravel/database/factories/UserFactory.php
@@ -3,21 +3,20 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->unique()->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('asdasdasd'),
+            'type' => 'client',
+            'photo' => $this->faker->imageUrl(360, 360, 'user', true, 'user'),
+            'is_active' => true,
         ];
     }
 }
